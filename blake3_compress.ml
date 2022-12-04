cf7a58a7298f7341f5188e25f86ae24012d6ea80
K <117524908+0x0FFH@users.noreply.github.com>
2022-12-04 05:05:05 +0200

    Update blake3_compress.ml

--- blake3_compress.ml
+++ blake3_compress.ml
@@ -1,10 +1,5 @@
-
-open Blake3_constants
-open Blake3_mix
-
-(* The compression function for the hash. *)
 let compress state message =
-  let s = Array.make 16 0l in
+  let s = Array.make 16 0L in
   for i = 0 to 7 do
     s.(i) <- state.(i)
   done;
@@ -14,3 +9,25 @@ let compress state message =
   for i = 0 to 15 do
     s.(i) <- s.(i) lxor (Int64.of_int message.(i))
   done;
+  let a, b, c, d = g s 0 1 2 3 4 5 6 7 in
+  let a, b, c, d = g s a b c d 0 9 10 11 in
+  let a, b, c, d = g s a b c d 4 13 14 15 in
+  let a, b, c, d = g s a b c d 8 1 2 3 in
+  let a, b, c, d = g s a b c d 12 5 6 7 in
+  let a, b, c, d = g s a b c d 0 13 14 15 in
+  let a, b, c, d = g s a b c d 4 9 10 11 in
+  let a, b, c, d = g s a b c d 8 5 6 7 in
+  let a, b, c, d = g s a b c d 12 1 2 3 in
+  let a, b, c, d = g s a b c d 0 13 14 15 in
+  let a, b, c, d = g s a b c d 4 9 10 11 in
+  let a, b, c, d = g s a b c d 8 5 6 7 in
+  let a, b, c, d = g s a b c d 12 1 2 3 in
+  let a, b, c, d = g s a b c d 0 13 14 15 in
+  let a, b, c, d = g s a b c d 4 9 10 11 in
+  let a, b, c, d = g s a b c d 8 5 6 7 in
+  let a, b, c, d = g s a b c d 12 1 2 3 in
+  let a, b, c, d = g s a b c d 0 9 10 11 in
+  let a, b, c, d = g s a b c d 4 5 6 7 in
+  for i = 0 to 7 do
+    state.(i) <- state.(i) lxor s.(i) lxor s.(i+8)
+  done
