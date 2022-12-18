d6fb5018387ff4788d32da545b6340cbc1620975
K <117524908+0x0FFH@users.noreply.github.com>
2022-12-18 13:39:06 +0200

    Update blake3_implementation.ml

--- blake3_implementation.ml
+++ blake3_implementation.ml
@@ -28,13 +28,18 @@ let int_of_blake3_flags flag =
   | DERIVE_KEY_CONTEXT -> 32
   | DERIVE_KEY_MATERIAL -> 64
 
-
+let counter_low counter = counter
+  
+let counter_high counter = counter lsr 32
+                           
+let load32 src =
+  let p = src in
+  (p.(0) lsl 0) lor (p.(1) lsl 8) lor (p.(2) lsl 16) lor (p.(3) lsl 24)
 
 let iv =
   [| 0x6A09E667L; 0xBB67AE85L; 0x3C6EF372L; 0xA54FF53AL;
-     0x510E527FL; 0x9B05688CL; 0x1F83D9ABL; 0x5BE0CD19L |]
-     
-     
+     0x510E527FL; 0x9B05688CL; 0x1F83D9ABL; 0x5BE0CD19L |] 
+  
 let msg_schedule =
   [|
     [| 0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12; 13; 14; 15 |];
@@ -45,7 +50,6 @@ let msg_schedule =
     [| 9; 14; 11; 5; 8; 12; 15; 1; 13; 3; 0; 10; 2; 6; 4; 7 |];
     [| 11; 15; 5; 0; 1; 9; 8; 6; 14; 10; 2; 12; 3; 4; 7; 13 |]
   |]
-
 
 let rotr32 (w: int32) (c: int32) : int32 = 
   Int32.(logor (shift_right w (to_int c)) (shift_left w (32 - to_int c))) ;;
@@ -69,9 +73,13 @@ let round_fn state msg round =
   g state 0 5 10 15 msg.(schedule.(8)) msg.(schedule.(9));
   g state 1 6 11 12 msg.(schedule.(10)) msg.(schedule.(11));
   g state 2 7 8 13 msg.(schedule.(12)) msg.(schedule.(13));
-  g state 3 4 9 14 msg.(schedule.(14)) msg.(schedule.(15))
-
-let load32 src =
-  let p = src in
-  (p.(0) lsl 0) lor (p.(1) lsl 8) lor (p.(2) lsl 16) lor (p.(3) lsl 24)
-
+  g state 3 4 9 14 msg.(schedule.(14)) msg.(schedule.(15)) 
+                                                         
+let compress_pre state cv block block_len counter flags =
+  let block_words = Array.init 16 (fun i ->
+      let offset = 4 * i in
+      load32 block.(offset)) in
+  Array.blit cv 0 state 0 8;
+  Array.blit iv 0 state 8 4; 
+  state.(14) <- block_len;
+  state.(15) <- flags;
