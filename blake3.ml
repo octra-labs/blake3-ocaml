0f11280912283a39801bfe0e064b32c1677315f8
K <117524908+lambda0xE@users.noreply.github.com>
2023-01-13 03:22:50 +0000

    Update blake3.ml

--- blake3.ml
+++ blake3.ml
@@ -3,6 +3,13 @@ let blake3_out_len = 32
 let blake3_block_len = 64
 let blake3_chunk_len = 1024
 let blake3_max_depth = 54
+let chunk_start = 1 lsl 0
+let chunk_end = 1 lsl 1
+let parent = 1 lsl 2
+let root = 1 lsl 3
+let keyed_hash = 1 lsl 4
+let derive_key_context = 1 lsl 5
+let derive_key_material = 1 lsl 6
 
 type blake3_chunk_state = {
   cv: int32 array;
@@ -26,4 +33,18 @@ let chunk_state_reset (self: blake3_chunk_state) (key: int32 array) (chunk_count
   self.chunk_counter <- chunk_counter;
   self.blocks_compressed <- 0;
   self.buf <- Bytes.make blake3_block_len '\x00';
-  self.buf_len <- 0;
+  self.buf_len <- 0
+
+let chunk_state_len (self: blake3_chunk_state) : int =
+  (blake3_block_len * self.blocks_compressed) + self.buf_len 
+
+let chunk_state_fill_buf (self: blake3_chunk_state) (input: bytes) (input_len: int) : int =
+  let take = blake3_block_len - self.buf_len in
+  let take = if take > input_len then input_len else take in
+  let dest = Bytes.sub self.buf self.buf_len take in
+  Bytes.blit input 0 dest 0 take;
+  self.buf_len <- self.buf_len + take;
+  take
+
+let chunk_state_maybe_start_flag (self: blake3_chunk_state) : int =
+  if self.blocks_compressed = 0 then chunk_start else 0
